7fec53a7786582a67a6b368d2d71031955b02499
Jake Sant <jake.sant.18@um.edu.mt>
2021-02-04 23:56:17 +0100

    Added vector and sign variables, created Empty construtor.

--- bigchungint.hpp
+++ bigchungint.hpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <vector>
 
 template <int T>
-class bigchungint
+class Bigchungint
 {
+private:
+    bool sign;
+    std::vector<> vec;
+
+public:
+    Bigchungint();
+
 
 }
+
+//https://stackoverflow.com/questions/4421706/what-are-the-basic-rules-and-idioms-for-operator-overloading
