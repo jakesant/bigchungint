3dc65b3df5a13337d1c7089130a5984fbb6731ac
Jake Sant <jake.sant.18@um.edu.mt>
2021-02-18 13:31:08 +0100

    Overloading ++ increment. Need to adjust for overflows

--- bigchungint.cpp
+++ bigchungint.cpp
@@ -5,4 +5,8 @@ int main()
     Bigchungint<5> num(10);
 
     std::cout << "Number is " << num << '\n';
+
+    num++;
+    
+    std::cout << "Number is now " << num << '\n';
 }
--- bigchungint.hpp
+++ bigchungint.hpp
@@ -30,6 +30,28 @@ public:
         vec = bci.vec;
     }
 
+    Bigchungint& operator++() //Overloads Preincrement ++ operator
+    {
+        for (unsigned int i = T-1; i < this->vec.size(); --i)
+        {
+            if (this->vec[i] == 0)
+            {
+                this->vec[i] = 1;
+                break;
+            }
+            this->vec[i] = 0;
+        }
+
+        return *this;
+    }
+
+    Bigchungint operator++(const int) //Overloads Postincrement ++ operator
+    {
+        Bigchungint<T> temp = *this;
+        ++*this;
+        return temp;
+    }
+
     friend std::ostream& operator<< <T>(std::ostream &out, const Bigchungint &bci);
 
 };
